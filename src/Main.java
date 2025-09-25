9d028ce14ddaf9113c79c01fd37d0f688d2f3861
Erik Petrov <erik.petrov@ut.ee>
2025-09-25 15:33:24 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/***********************************
+ * Algoritmid ja andmestruktuurid. LTAT.03.005
+ * 2025/2026 sügissemester
+ *
+ * Kodutöö nr 1
+ * Teema: Algoritmide vordsus(?)
+ *
+ * Autor: Erik Petrov
+ **********************************/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/Sorter.java
@@ -0,0 +1,19 @@
+public class Sorter {
+    /**
+     * Bubble sort implementation
+     * @param int array to sort
+     * @return sorted array
+     */
+    public static void BubbleSort(int[] arr ) {
+        //TODO: bubble sort
+    }
+
+    /**
+     * Merge sort implementation
+     * @param int array to sort
+     * @return sorted array
+     */
+    public static void MergeSort(int[] arr){
+        //TODO: merge sort
+    }
+}
