30408f48020a7866120e6577c0c994caa5c50ac3
Erik Petrov <erikpetrov23@gmail.com>
2025-09-25 21:24:39 +0300

    HW done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Random;
 /***********************************
  * Algoritmid ja andmestruktuurid. LTAT.03.005
  * 2025/2026 sügissemester
@@ -10,6 +12,56 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] arrSizes = {0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144};
+        long[][] arrResults = new long[Sorter.Functions.values().length][arrSizes.length]; //results in nanoseconds
+        for (Sorter.Functions i : Sorter.Functions.values()) { //bubble then merge
+            for (int j = 0; j < arrSizes.length; j++) { //every array size
+                int[] a = generateArray(arrSizes[j]);
+                long startTime = System.nanoTime();
+                if(i.equals(Sorter.Functions.BubbleSort)) {
+                    Sorter.BubbleSort(a);
+                } else{
+                    Sorter.MergeSort(a, 0, a.length-1);
+                }
+                assert isSorted(a) : i.name()+" failed to sort array["+j+"]\na="+Arrays.toString(a);
+                arrResults[i.ordinal()][j] = (System.nanoTime() - startTime);
+            }
+        }
+        System.out.println(Arrays.deepToString(arrResults));
+    }
+
+    /**
+     * Generates an array of random integers, when 1024 -> makes an ordered array
+     * @param size array size
+     * @return generated array
+     */
+    public static int[] generateArray(int size) {
+        if (size == 1024){ //just an already sorted array at the end
+            int[] arr = new int[1024];
+            for (int i = 0; i < arr.length; i++){
+                arr[i] = i;
+            }
+            return arr;
+        }
+        int[] arr = new int[size];
+        Random rnd = new Random();
+        for (int i = 0; i < size; i++) {
+            arr[i] = rnd.nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+        return arr;
+    }
+
+    /**
+     * Checks if the array is sorted
+     * @param arr the array
+     * @return array sorted status
+     */
+    public static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/Sorter.java
+++ src/Sorter.java
@@ -1,19 +1,88 @@
+import java.util.Arrays;
+
 public class Sorter {
     /**
      * Bubble sort implementation
-     * @param int array to sort
+     * @param arr array to sort
      * @return sorted array
      */
-    public static void BubbleSort(int[] arr ) {
-        //TODO: bubble sort
+    public static int[] BubbleSort(int[] arr ) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
     }
 
     /**
      * Merge sort implementation
-     * @param int array to sort
-     * @return sorted array
+     * <a href="https://www.programiz.com/dsa/merge-sort">source</a>
+     *
+     * @param arr array to sort
+     * @param f   index from
+     * @param t   index to
      */
-    public static void MergeSort(int[] arr){
-        //TODO: merge sort
+    public static void MergeSort(int[] arr, int f, int t){
+        if (f < t) {
+            int mid = (t + f) / 2;
+
+            MergeSort(arr, f, mid);
+            MergeSort(arr, mid + 1, t);
+
+            merge(arr, f, mid, t);
+        }
+    }
+
+    public static void merge(int[] arr, int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int[] L = new int[n1];
+        int[] M = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[p + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            M[j] = arr[q + 1 + j];
+        }
+
+        int i, j, k;
+        i = 0;
+        j = 0;
+        k = p;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= M[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = M[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = M[j];
+            j++;
+            k++;
+        }
+    }
+
+    public enum Functions{
+        BubbleSort,
+        MergeSort
     }
 }
